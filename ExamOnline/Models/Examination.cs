74a978cb0670b95d17779790193224bc47fcf5b8
sebayang <ibrahim.sebayang@gmail.com>
2020-09-23 22:52:55 +0700

    Model Examination

---
+++ ExamOnline/Models/Examination.cs
@@ -0,0 +1,23 @@
+﻿using ExamOnline.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamOnline.Models
+{
+    [Table("tb_t_answer")]
+    public class Examination : BaseModel
+    {
+        [Key]
+        public string Id { get; set; }
+        public bool isDelete { get ; set; }
+        public string EmployeeId { get; set; }
+        public string SubjectId { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset RescheduleDate { get; set; }
+        public int Score { get; set; }
+    }
+}
