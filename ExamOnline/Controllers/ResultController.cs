599411912e49213e9ab3acf85dc6283167e269ed
sebayang <ibrahim.sebayang@gmail.com>
2020-10-01 17:41:28 +0700

    GAS

--- ExamOnline/Controllers/ResultController.cs
+++ ExamOnline/Controllers/ResultController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ExamOnline.Base;
+using ExamOnline.Context;
 using ExamOnline.Models;
 using ExamOnline.Repositories.Data;
 using Microsoft.AspNetCore.Http;
@@ -15,10 +16,11 @@ namespace ExamOnline.Controllers
     public class ResultController : BaseController<Examination, ExaminationRepository>
     {
         private ExaminationRepository _repo;
-
-        public ResultController(ExaminationRepository repo) : base(repo)
+        readonly MyContext _context;
+        public ResultController(ExaminationRepository repo , MyContext myconn) : base(repo)
         {
             _repo = repo;
+            _context = myconn;
         }
         [HttpGet("emp/{Id}")]
         public IActionResult GetByUserId(string Id)
@@ -31,7 +33,15 @@ namespace ExamOnline.Controllers
             else
             {
                 var score = _repo.GetByUser(Id);
-
+                foreach (var item in score)
+                {
+                    //if (item.Score == 0)
+                    //{
+                        var result = setScore(item.Id);
+                    //}
+                    
+                }
+                
                 if (score == null)
                 {
                     return BadRequest("Data Not Found");
@@ -41,6 +51,23 @@ namespace ExamOnline.Controllers
                     return Ok(score);
                 }
             }
+        }
+
+        public async Task<IActionResult> setScore(string id)
+        {
+            var examination = _context.Examinations.FirstOrDefault(x => x.Id == id);
+            var listAnswer = _context.Answer.Where(x => x.ExamId == examination.Id).ToList();
+            int count = 0;
+            foreach (var item in listAnswer)
+            {
+                if (item.Status == true)
+                {
+                    count = count + 10;
+                }
+            }
+            examination.Score = count;
+            var data = await _repo.Update(examination);
+            return  Ok();
         }
     }
 
