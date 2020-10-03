cd68ade40aa30dce299dfc6768507c1519d1b076
Jepri <55873259+jeffreymas@users.noreply.github.com>
2020-10-03 16:51:58 +0700

    add Model Section

--- ExamOnline/Migrations/20201003084115_JepSection.Designer.cs
+++ ExamOnline/Migrations/20201003084115_JepSection.Designer.cs
@@ -4,14 +4,16 @@ using ExamOnline.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ExamOnline.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20201003084115_JepSection")]
+    partial class JepSection
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
